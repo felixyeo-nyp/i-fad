2d6a98f7992a971fa8b7554b3add5884693028b3
casdllum <callumngkl@gmail.com>
2024-07-26 14:32:35 +0800

    Changed UI and added stuff

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash, Response, jsonify
-from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, ValidationError
+from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, ValidationError, PasswordField
 import sys
-
-from Forms import configurationForm, emailForm
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import shelve
+from flask_wtf import FlaskForm
+from Forms import configurationForm, emailForm, LoginForm, RegisterForm
 
 j = datetime.datetime.now()
 print(j)
@@ -73,13 +77,11 @@ class FreshestFrame(threading.Thread):
                 if (sequence_number) > 0:
                     self.pellets_num = sequence_number
 
-
                 # Wait until the latest frame's sequence number is greater than or equal to sequence_number
                 rv = self.condition.wait_for(lambda: self.pellets_num >= sequence_number, timeout=timeout) # if there is a pellets. should get "true"
                 if not rv:
                     return (self.pellets_num, self.frame)  # Return the latest frame if timeout occurs
             return (self.pellets_num, self.frame)  # Return the latest frame
-
 
 # define the id "1" for pellets
 # do note that in the pth file, the pellet id also is 1
@@ -88,10 +90,8 @@ class_labels = {
     2: 'Fecal Matters'
 }
 
-
 # pth file where you have defined on roboflow
 model_path = './best_model.pth'
-
 
 def create_model(num_classes, pretrained=False, coco_model=False):
     if pretrained:
@@ -123,9 +123,6 @@ def generate_frames():
 
     fresh = FreshestFrame(cap)
 
-
-
-
     # Load the Faster R-CNN model from the .pth file
     num_classes = 2  # Assuming 2 classes for 'Pellets' and background
     model = load_model(model_path, num_classes)
@@ -138,7 +135,6 @@ def generate_frames():
     global object_count
     object_count = {1: 0}
 
-
     feeding = False
     feeding_timer = None
 
@@ -146,39 +142,28 @@ def generate_frames():
     Time_Record_dict = db['Time_Record']
     db.close()
 
-
     setting = Time_Record_dict.get('Time_Record_Info')
 
     hours, minutes = setting.get_first_timer().split(':')
     hours1, minutes1 = setting.get_second_timer().split(':')
 
-
     first_feeding_time = int(hours)
     first_feeding_time_min = int(minutes)
-
 
     second_feeding_time = int(hours1)
     second_feeding_time_min = int(minutes1)
 
     confidence = float(setting.get_confidence())/100
 
-
     showing_timer = None
     line_chart_timer, email_TF = (None,False)
     desired_time = None
-
     formatted_desired_time = None
     current_datetime = datetime.now()
-
-
-
-
-
 
     while True:
         # Process the predictions and update object count
         temp_object_count = {1: 0}  # Initialize count for the current frame
-
 
         current_time = datetime.now().time()
         if (current_time.hour == first_feeding_time or current_time.hour == second_feeding_time) and (current_time.minute == first_feeding_time_min or current_time.minute == second_feeding_time_min) and current_time.second == 0:
@@ -187,13 +172,9 @@ def generate_frames():
             showing_timer = None
             line_chart_timer = time.time()
 
-
-
         cnt, frame = fresh.read(sequence_number=object_count[1] + 1)
         if frame is None:
             break
-
-
         # Preprocess the frame
         img_tensor = torchvision.transforms.ToTensor()(frame).to(device)
         img_tensor = img_tensor.unsqueeze(0)
@@ -203,28 +184,20 @@ def generate_frames():
             predictions = model(img_tensor)
 
         # processed_labels = set()  # Keep track of processed labels
-
         for i in range(len(predictions[0]['labels'])):
             label = predictions[0]['labels'][i].item()
-
             # if label in processed_labels:
             #     continue
-
             # processed_labels.add(label)
-
             if label in class_labels:
                 box = predictions[0]['boxes'][i].cpu().numpy().astype(int) # used to define the size of the object
                 score = predictions[0]['scores'][i].item() #the probability of the object
-
-
-
                 # if label == 2 and score > 0.3:
                 #     # Draw bounding box and label on the frame
                 #     cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 0, 255),
                 #                   2)  # (0,255,0) is the color (blue, green, yellow)
                 #     cv2.putText(frame, f'{class_labels[label]}: {score:.2f}', (box[0], box[1] - 10),
                 #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
                 # 0.95 is the highest, while we are looking for 90% of the probability
                 if (label == 1 and score > confidence):
                     # Draw bounding box and label on the frame
@@ -232,14 +205,10 @@ def generate_frames():
                     cv2.putText(frame, f'{class_labels[label]}: {score:.2f}', (box[0], box[1] - 10),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-
                     temp_object_count[label] += 1
-
                     # Start feeding timer if pellets are detected
                     if label == 1 and feeding_timer is None and feeding:
                         feeding_timer = time.time()
-
-
 
         # store the pellets number to the object count which is permanently
         for label, count in temp_object_count.items():
@@ -259,7 +228,6 @@ def generate_frames():
             # change to None when there is no pellets
             elif object_count[1] == 0:
                 feeding_timer = None
-
 
         # Display the frame with detections and object count
         for label, count in object_count.items():
@@ -324,8 +292,6 @@ def generate_frames():
                     cv2.putText(frame, formatted_desired_time, text_position, cv2.FONT_HERSHEY_SIMPLEX, 1.2,
                                 (0, 255, 0), 2)
 
-
-
                 elif ((current_time.hour <= second_feeding_time and current_time.minute <= second_feeding_time_min)) or (current_time.hour < second_feeding_time):
                     desired_time = current_datetime.replace(hour=second_feeding_time, minute=second_feeding_time_min, second=0,
                                                             microsecond=0)
@@ -334,9 +300,6 @@ def generate_frames():
                     text_size = cv2.getTextSize(formatted_desired_time, cv2.FONT_HERSHEY_SIMPLEX, 1.2, 3)[0]
                     text_position = (frame.shape[1] - text_size[0] - 10, 30 * 4)
                     cv2.putText(frame, formatted_desired_time, text_position, cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
-
-
-
 
                 else:
                     # Add one day to the current date and time
@@ -351,8 +314,6 @@ def generate_frames():
                     cv2.putText(frame, formatted_desired_time, text_position, cv2.FONT_HERSHEY_SIMPLEX, 1.2,
                                 (0, 0, 255), 2)
 
-
-
         ret, jpeg = cv2.imencode('.jpg', frame)
         if not ret:
             continue  # Continue to the next frame if encoding fails
@@ -362,16 +323,109 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-
     fresh.stop()
     cap.release()
 
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# User model
+class User(UserMixin):
+    def __init__(self, username, email, password):
+        self.id = username  # Use username as ID for simplicity
+        self.username = username
+        self.email = email
+        self.password = password
+
+# User loader callback for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    with shelve.open('users.db') as db:
+        user_data = db.get(user_id)
+        if user_data:
+            return User(user_data['username'], user_data['email'], user_data['password'])
+        else:
+            return None
+
+# Default route to redirect to login page
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+# Function to open shelve safely
+def open_shelve(filename, mode='c'):
+    try:
+        shelf = shelve.open(filename, mode)
+        return shelf
+    except Exception as e:
+        print(f"Error opening shelve: {e}")
+        return None
+# Routes for Registration and Login
+# Routes for Registration and Login using shelve
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()  # Create an instance of RegisterForm
+
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        confirm_password = form.confirm_password.data
+
+        # Check if the username already exists in the database
+        with shelve.open('users.db', 'c') as db:
+            if username in db:
+                flash('Username already exists', 'danger')
+            else:
+                # If username is not already registered, proceed with registration
+                hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+                new_user = User(username, email, hashed_password)
+                db[username] = {'username': new_user.username, 'email': new_user.email, 'password': new_user.password}
+                flash('You are now registered and can log in', 'success')
+                return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        try:
+            # Attempt to open the shelve database file
+            with shelve.open('users.db', 'c') as db:
+                if username in db:
+                    stored_password_hash = db[username]['password']
+                    if check_password_hash(stored_password_hash, password):
+                        # Passwords match, login successful
+                        user = User(username, db[username]['email'], stored_password_hash)
+                        login_user(user)
+                        flash('You are now logged in', 'success')
+                        return redirect(url_for('dashboard'))
+                    else:
+                        flash('Invalid login credentials', 'danger')
+                else:
+                    flash('Invalid login credentials', 'danger')
+        except Exception as e:
+            flash(f'Error: {str(e)}', 'danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
 
 from flask import Flask, flash, render_template, request, redirect, session, url_for
 import shelve, re
-
 
 # Helper function to get settings from the database
 def get_settings():
@@ -427,8 +481,7 @@ def get_interval():
         app.logger.error(f"An error occurred while retrieving interval: {str(e)}")
         return jsonify({'error': 'An error occurred while retrieving interval.'}), 500
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     edit_form = configurationForm(request.form)
     db = shelve.open('settings.db', 'r')
@@ -480,7 +533,6 @@ def export_data():
 
     return send_file(file_path, as_attachment=True, download_name='consumption_data.xlsx')
 
-
 @app.route('/pellet_counts')
 def pellet_counts():
     global object_count
@@ -498,7 +550,6 @@ def pellet_counts():
     }
 
     return jsonify(data)
-
 
 import re
 
@@ -552,7 +603,6 @@ def update_setting():
         setting.confidence.data = j.get_confidence()
         return render_template('settings.html', form=setting)
 
-
 @app.route('/update/email', methods=['GET', 'POST'])
 def update_email_settings():
     setting = emailForm(request.form)
@@ -583,14 +633,12 @@ def update_email_settings():
         setting.days.data = j.get_days()
         return render_template('email_settings.html', form=setting)
 
-
 @app.route('/data_analysis/feeding_time')
 def line_chart():
     db = shelve.open('line_chart_data.db', 'r')
     Line_Chart_Data_dict = db.get('Line_Chart_Data',{})
     days = []
     timer = []
-
 
     # Iterate over the last seven days
     for i in range(6, -1, -1):
@@ -605,11 +653,9 @@ def line_chart():
             days.append(object.get_date())
             timer.append(object.get_timeRecord())
 
-
     # Print or process the data as needed
     for day, time_record in zip(days, timer):
         print(f"{day}: {time_record}")
-
 
     db.close()
     return render_template('feeding_line_chart.html', days = days, timer = timer)
@@ -657,7 +703,6 @@ def video_feed():
         print(f"Error: {e}")
         return "Error generating video feed"
 
-
 def sending_email():
     import os
     from email.message import EmailMessage
@@ -688,8 +733,6 @@ def sending_email():
         current_date = (datetime.today() - timedelta(days=i)).strftime("%Y-%m-%d")
         comparison_date = (datetime.today() - timedelta(days=i+1)).strftime("%Y-%m-%d")
 
-
-
         # Check if the current date exists in the Line_Chart_Data_dict
         if current_date in Line_Chart_Data_dict:
             # Get the Line_Chart_Data object for the current date
@@ -717,16 +760,12 @@ def sending_email():
         for day, time_record in zip(date_list, timer):
             msg += f'<p>{day}: <strong>{int(time_record)} seconds</strong></p>'
 
-
         # eeko murx wrcu zepp
         subject = 'Alert!!!'
         body = (
             f'<p>Regarding the past three days time record, the graph has shown a decreasing trend. </p>\n{msg}'
 
         )
-
-
-
 
         em = EmailMessage()
         em['From'] = email_sender
@@ -742,9 +781,6 @@ def sending_email():
             smtp.login(email_sender, email_password)
             smtp.sendmail(email_sender, email_receiver, em.as_string())
 
-
-
-
 from OOP import *
 
 if __name__ == '__main__':
@@ -753,13 +789,11 @@ if __name__ == '__main__':
         print("Attempting to open the database file for reading.")
         print("Database file opened for reading.")
 
-
         db = shelve.open('settings.db', 'r')
         # Attempt to get 'Time_Record' from db, if not found, initialize with empty dictionary
         Time_Record_dict = db.get('Time_Record',{})
         Email_dict = db.get('Email_Data', {})
         db.close()
-
 
         db = shelve.open('line_chart_data.db', 'w')
         Line_Chart_Data_dict = db.get('Line_Chart_Data',{})  # Attempt to get 'Time_Record' from db, if not found, initialize with empty dictionary
@@ -769,8 +803,6 @@ if __name__ == '__main__':
             linechart = Line_Chart_Data(current_date, 0)
             Line_Chart_Data_dict[current_date] = linechart
             db['Line_Chart_Data'] = Line_Chart_Data_dict
-
-
 
         ###### test code ####
         today = datetime.today()
@@ -799,12 +831,8 @@ if __name__ == '__main__':
             # print(Line_Chart_Data_dict)
             # sending_email()
 
-
-
-
         db['Line_Chart_Data'] = Line_Chart_Data_dict
         db.close()
-
 
         # db = shelve.open('line_chart_data_pellets.db', 'r')
         # Line_Chart_Data_pellets_dict = db.get('Line_Chart_Data_Pellets',{})  # Attempt to get 'Time_Record' from db, if not found, initialize with empty dictionary
@@ -813,14 +841,11 @@ if __name__ == '__main__':
         #     db['Line_Chart_Data_Pellets'] = Line_Chart_Data_pellets_dict
         #
         # db.close()
-
-
         # the date you have
         print('the Date you have:\n-------------------------------------------------------')
         for i in Line_Chart_Data_dict:
             print(i,': ', (Line_Chart_Data_dict.get(i).get_timeRecord()))
         print('-------------------------------------------------------')
-
 
     except:
         # If the file doesn't exist, create a new one
@@ -844,7 +869,6 @@ if __name__ == '__main__':
         # close the db
         db.close()
 
-
         #  create the line chart database
         db = shelve.open('line_chart_data.db', 'c')
         # Get today's date
@@ -861,8 +885,5 @@ if __name__ == '__main__':
         db['Line_Chart_Data'] = Line_Chart_Data_dict
         db.close()
 
-
-
-
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
