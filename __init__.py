a28c37c3bc1971dd47a7809539b62a378ad276a5
casdllum <callumngkl@gmail.com>
2024-08-01 16:36:42 +0800

    video

--- __init__.py
+++ __init__.py
@@ -120,8 +120,11 @@ def generate_frames():
     cap =cv2.VideoCapture('./sample.mp4')
     #cap =cv2.VideoCapture(0)
 
+    cap.set(cv2.CAP_PROP_FPS, 30)
+
     fresh = FreshestFrame(cap)
 
+    # Load the Faster R-CNN model from the .pth file
     num_classes = 2  # Assuming 2 classes for 'Pellets' and background
     model = load_model(model_path, num_classes)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -139,14 +142,15 @@ def generate_frames():
     db.close()
 
     setting = Time_Record_dict.get('Time_Record_Info')
+
     hours, minutes = setting.get_first_timer().split(':')
     hours1, minutes1 = setting.get_second_timer().split(':')
 
     first_feeding_time = int(hours)
     first_feeding_time_min = int(minutes)
-
     second_feeding_time = int(hours1)
     second_feeding_time_min = int(minutes1)
+
     confidence = float(setting.get_confidence()) / 100
 
     showing_timer = None
@@ -155,8 +159,13 @@ def generate_frames():
     formatted_desired_time = None
     current_datetime = datetime.now()
 
+    frame_skip = 2  # Skip every other frame
+    frame_count = 0
+
     while True:
+        # Process the predictions and update object count
         temp_object_count = {1: 0}  # Initialize count for the current frame
+
         current_time = datetime.now().time()
         if (current_time.hour == first_feeding_time or current_time.hour == second_feeding_time) and (
                 current_time.minute == first_feeding_time_min or current_time.minute == second_feeding_time_min) and current_time.second == 0:
@@ -169,67 +178,94 @@ def generate_frames():
         if frame is None:
             break
 
+        # Skip frames
+        frame_count += 1
+        if frame_count % frame_skip != 0:
+            continue
+
+        # Reduce frame resolution
+        frame = cv2.resize(frame, (640, 360))
+
+        # Preprocess the frame
         img_tensor = torchvision.transforms.ToTensor()(frame).to(device)
         img_tensor = img_tensor.unsqueeze(0)
 
+        # Perform inference
         with torch.no_grad():
             predictions = model(img_tensor)
 
         for i in range(len(predictions[0]['labels'])):
             label = predictions[0]['labels'][i].item()
             if label in class_labels:
-                box = predictions[0]['boxes'][i].cpu().numpy().astype(int)
-                score = predictions[0]['scores'][i].item()
+                box = predictions[0]['boxes'][i].cpu().numpy().astype(int)  # used to define the size of the object
+                score = predictions[0]['scores'][i].item()  # the probability of the object
 
-                if (label == 1 and score > confidence):
-                    cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+                if label == 1 and score > confidence:
+                    # Draw bounding box and label on the frame
+                    cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0),
+                                  2)  # (0,255,0) is the color (blue, green, yellow)
                     cv2.putText(frame, f'{class_labels[label]}: {score:.2f}', (box[0], box[1] - 10),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
                     temp_object_count[label] += 1
 
+                    # Start feeding timer if pellets are detected
                     if label == 1 and feeding_timer is None and feeding:
                         feeding_timer = time.time()
 
+        # store the pellets number to the object count which is permanently
         for label, count in temp_object_count.items():
-            if label == 1:
+            if label == 1:  # Assuming label 1 represents 'Pellets'
                 object_count[label] = count
 
+        # Check feeding timer and switch to stop feeding if required
         if feeding_timer is not None and feeding:
             elapsed_time = (time.time() - feeding_timer)
+            print(f'elapsed time: {elapsed_time:.3f}')
+
             if elapsed_time > int(setting.get_seconds()) and sum(object_count.values()) > int(setting.get_pellets()):
                 feeding = False
                 feeding_timer = None
                 showing_timer = time.time()
+
+            # change to None when there are no pellets
             elif object_count[1] == 0:
                 feeding_timer = None
 
+        # Display the frame with detections and object count
         for label, count in object_count.items():
             text = f'{class_labels[label]} Count: {count}'
             text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1.2, 2)[0]
             text_position = (frame.shape[1] - text_size[0] - 10, 30 * (label + 1))
             cv2.putText(frame, text, text_position, cv2.FONT_HERSHEY_SIMPLEX, 1.2, (200, 255, 255), 2)
 
+        # Display feeding or stop feeding text just below the object counter
         text_position_feed = (frame.shape[1] - text_size[0] - 10, 30 * (max(object_count.keys()) + 1))
 
         if feeding:
-            cv2.putText(frame, "Feeding...", text_position_feed, cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
+            cv2.putText(frame, "Feeding...", text_position_feed,
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
         else:
             if showing_timer is not None:
                 i = time.time() - showing_timer
+
                 if i > 3:
                     showing_timer = None
                     j = time.time() - line_chart_timer
+
                     line_chart_timer = None
 
                     db = shelve.open('line_chart_data.db', 'w')
                     Line_Chart_Data_dict = db['Line_Chart_Data']
+
                     current_date = datetime.today().strftime("%Y-%m-%d")
 
                     if current_date in Line_Chart_Data_dict:
                         Line_chart_objects = Line_Chart_Data_dict.get(current_date)
                         Line_chart_objects.set_timeRecord(j + Line_chart_objects.get_timeRecord())
                         Line_Chart_Data_dict[current_date] = Line_chart_objects
-                    else:
+                    elif current_date not in Line_Chart_Data_dict:
+                        print(current_time, " is not in the dictionary. creating a new one...")
                         new_object = Line_Chart_Data(current_date, j)
                         Line_Chart_Data_dict[current_date] = new_object
                     db['Line_Chart_Data'] = Line_Chart_Data_dict
@@ -237,42 +273,37 @@ def generate_frames():
 
                     if (current_time.hour >= first_feeding_time) and (
                             current_time.hour >= second_feeding_time and current_time.minute > second_feeding_time_min):
+                        print('sending email feature')
                         sending_email()
 
                     for today_date in Line_Chart_Data_dict:
                         Line_chart_objects = Line_Chart_Data_dict.get(today_date)
                         print(Line_chart_objects.get_date(), ': ', Line_chart_objects.get_timeRecord())
+
                     print('running in website')
                 else:
-                    cv2.putText(frame, "Stop Feeding", text_position_feed, cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255),
-                                2)
+                    cv2.putText(frame, "Stop Feeding", text_position_feed,
+                                cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
             else:
                 if (
                         current_time.hour <= first_feeding_time and current_time.minute <= first_feeding_time_min) or current_time.hour < first_feeding_time:
                     desired_time = current_datetime.replace(hour=first_feeding_time, minute=first_feeding_time_min,
-                                                            second=0, microsecond=0)
+                                                            second=0,
+                                                            microsecond=0)
                     formatted_desired_time = 'Next Round: ' + desired_time.strftime("%I:%M %p")
 
                     text_size = cv2.getTextSize(formatted_desired_time, cv2.FONT_HERSHEY_SIMPLEX, 1.2, 3)[0]
                     text_position = (frame.shape[1] - text_size[0] - 10, 30 * 4)
                     cv2.putText(frame, formatted_desired_time, text_position, cv2.FONT_HERSHEY_SIMPLEX, 1.2,
                                 (0, 255, 0), 2)
+
                 elif (
                 (current_time.hour <= second_feeding_time and current_time.minute <= second_feeding_time_min)) or (
                         current_time.hour < second_feeding_time):
                     desired_time = current_datetime.replace(hour=second_feeding_time, minute=second_feeding_time_min,
-                                                            second=0, microsecond=0)
+                                                            second=0,
+                                                            microsecond=0)
                     formatted_desired_time = 'Next Round: ' + desired_time.strftime("%I:%M %p")
-
-                    text_size = cv2.getTextSize(formatted_desired_time, cv2.FONT_HERSHEY_SIMPLEX, 1.2, 3)[0]
-                    text_position = (frame.shape[1] - text_size[0] - 10, 30 * 4)
-                    cv2.putText(frame, formatted_desired_time, text_position, cv2.FONT_HERSHEY_SIMPLEX, 1.2,
-                                (0, 255, 0), 2)
-                else:
-                    next_day = current_datetime + timedelta(days=1)
-                    desired_time = next_day.replace(hour=first_feeding_time, minute=first_feeding_time_min, second=0,
-                                                    microsecond=0)
-                    formatted_desired_time = 'Tomorrow at: ' + desired_time.strftime("%I:%M %p")
 
                     text_size = cv2.getTextSize(formatted_desired_time, cv2.FONT_HERSHEY_SIMPLEX, 1.2, 3)[0]
                     text_position = (frame.shape[1] - text_size[0] - 10, 30 * 4)
@@ -283,11 +314,6 @@ def generate_frames():
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-        gc.collect()
-
-    cap.release()
-    torch.cuda.empty_cache()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
